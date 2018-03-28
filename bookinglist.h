e2c3130476b1f916e795abfda404119a3a613da5
Ian Schafer <ianschafer@gmail.com>
2018-03-29 00:17:06 +0200

    Ongoing dev

---
+++ bookinglist.h
@@ -0,0 +1,93 @@
+#include <QList>
+#include <QString>
+#include <QStringList>
+#include <QDate>
+#include <QTextStream>
+
+//start id=baseclassdef
+class Booking {
+friend QTextStream& operator <<(QTextStream& s, Booking& b)
+{
+    s << b.m_ArrivalDate << "/n" << b.m_DepartureDate << endl;
+    return s;
+}
+public:
+    Booking (class Person c, QDate a, QDate d); //"class" indicates Forward class declaration
+    Booking(const Booking&);
+    Booking(QStringList& bookinglist);
+    const Booking& operator=(const Booking&);
+    virtual ~Booking();
+    virtual double rate();
+    virtual QString toString() const;
+    bool booked(QDate d);
+    static const double  SINGLE_PPPN;
+    static const double  SHARING_PPPN;
+
+private:
+    Person  *m_Contact;
+    QDate   m_ArrivalDate;
+    QDate   m_DepartureDate;
+};
+//end
+
+//start id=derivedclassdefs
+class Person : public Booking {
+
+public:
+    Person (QString n, QString c, QString e);
+    Person(const Person& person);
+    Person(QStringList& bookinglist);
+    virtual QString toString() const;
+
+private:
+   QString m_Name;
+   QString m_ContactNo;
+   QString m_Email;
+};
+
+class Single : public Person {
+
+public:
+    Single(Person c, QDate a, QDate d, Person g);// : m_Guest(g) {}
+    Single(const Single& single);
+    Single(QStringList& bookinglist);
+    QString toString() const;
+    double rate();
+
+private:
+    Person  m_Guest;
+};
+
+class Sharing : public Person {
+
+public:
+    Sharing(Person c, QDate a, QDate d, class Person g1, class Person g2);// : m_Guest1(g1), m_Guest2(g2) {}
+    Sharing(const Sharing& sharing);
+    Sharing(QStringList& bookinglist);
+    QString toString() const;
+    double rate();
+
+private:
+   Person  m_Guest1;
+   Person  m_Guest2;
+};
+//end
+
+//start id=QListdef
+class BookingList : public QList<Booking*> {
+public:
+   static const int NO_OF_ROOMS = 10;    //Set fixed number of rooms
+
+   BookingList();
+   ~BookingList();                             /* A container of pointers must have a destructor! */
+   int roomsAvailable();
+   bool vacancy(QDate a, QDate d);
+   void addBooking();
+   //Booking* addBooking(Person c, QDate a, QDate d, Person *g1, Person *g2);
+   void deleteAll(); //Call at end of client pgm to prevent mem leaks
+
+private:
+   BookingList(const BookingList&);
+   BookingList& operator=(const BookingList&);
+};
+//end
